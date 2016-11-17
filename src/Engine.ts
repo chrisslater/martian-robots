c2944c729fe34580861079dfd00b4f2d8227e6c5
Christopher Slater <chris@snapper.fish>
2016-11-17 14:25:29 +0000

    Update start to return on lost and update type

--- src/Engine.ts
+++ src/Engine.ts
@@ -2,8 +2,8 @@ import * as Types from './types';
 
 export class Engine implements Types.Engine {
   protected grid: Types.Grid;
-  protected startPosition: Types.CoordinateTuple = [0 , 0];
-  protected instructions: Types.Direction[] = [];
+  protected startPosition: Types.CoordinateWithDirectionTuple = [0 , 0, 'N'];
+  protected instructions: Types.Movement[] = [];
 
   constructor(grid: Types.Grid) {
     this.grid = grid;
@@ -13,11 +13,11 @@ export class Engine implements Types.Engine {
     this.grid.setGridSize(coordinate);
   }
 
-  public setStartPosition(coordinate: Types.CoordinateTuple) {
+  public setStartPosition(coordinate: Types.CoordinateWithDirectionTuple) {
     this.startPosition = coordinate;
   }
 
-  public setInstructions(instructions: Types.Direction[]) {
+  public setInstructions(instructions: Types.Movement[]) {
     this.instructions = instructions;
   }
 
@@ -25,14 +25,24 @@ export class Engine implements Types.Engine {
     this.grid.setDeadZones(deadZones);
   }
 
-  public start(): Types.CoordinateTuple[] {
+  public start(): Types.CoordinateWithDirectionTuple[] {
     let position = this.startPosition;
-    return this.instructions.map((instruction) =>
-      this.generateMove(instruction, position)
-    );
+    const movements = [ position ];
+
+    for (let i = 0; i < this.instructions.length; i++) {
+      const newPosition = this.generateMove(this.instructions[i], position);
+      movements.push(newPosition);
+      position = newPosition;
+
+      if (newPosition.includes('LOST')) {
+        return movements;
+      }
+    }
+
+    return movements;
   }
 
-  protected generateMove(instruction: Types.Direction, position: Types.CoordinateTuple) {
+  protected generateMove(instruction: Types.Movement, position: Types.CoordinateWithDirectionTuple) {
     return this.grid.generateMove(instruction, position);
   }
 }
